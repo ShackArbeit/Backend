d57a4734e20d56a359257668de452665e83ac360
Shack <g0972222165@gmail.com>
2024-09-08 09:34:09 +0800

    Add Receive Message

--- backend/settings.py
+++ backend/settings.py
@@ -61,6 +61,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "backend.wsgi.application"
+ASGI_APPLICATION='backend.asgi.application'
 
 
 # Database
@@ -114,3 +115,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+CHANNEL_LAYERS = {
+      "default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}
+}
--- chat/consumers.py
+++ chat/consumers.py
@@ -9,3 +9,32 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(self.room_name, self.channel_name)
 
         await self.accept()
+    async def disconnect(self,code):
+        await self.channel_layer.group_discard(self.room_name,self.channel_name)
+        self.close(self)
+    
+    async def receive(self,text_data):
+        print('收到訊息了!')
+        data_json=json.loads(text_data)
+        print(data_json)
+        event={'type':'send_message','message':data_json}
+        await self.channel_layer.group_send(self.room_name, event)
+    
+    async def send_message(self,event):
+        data=event['message']
+        await self.create_message(data=data)
+        response={
+            'sender':data['sender'],
+            'message':data['message']
+        }
+        await self.send(text_data=json.dumps({'message':response}))
+    @database_sync_to_async
+    def create_message(self,data):
+        get_room=Room.objects.get(room_name=data['room_name'])
+        if not Message.objects.filter(
+            message=data['message'],send=data['sender']
+        ).exists():
+            new_message=Message.objects.create(
+                room=get_room,message=data['message'],
+                sender=data['sender']
+            )
